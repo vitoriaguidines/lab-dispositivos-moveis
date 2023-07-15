f58139c69c11258d94055bd04c0d3179f51c0d1d
vitoriaguidines <vitoriaguidine@id.uff.br>
2023-07-15 20:20:45 -0300

    caixa transparente

--- app_catalogo/lib/interface/login.dart
+++ app_catalogo/lib/interface/login.dart
@@ -1,4 +1,3 @@
-//tela contem espaço para cadastro e cleber
 import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -15,42 +14,51 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Stack(
-      children: [
-        Container(
-          width: 328,
-          height: 193,
-          decoration: ShapeDecoration(
-            color: Color(0xFF262A2B),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(24),
+      body: Stack(
+        children: [
+          Container(
+            width: double.infinity,
+            height: double.infinity,
+            decoration: BoxDecoration(color: Colors.black),
+            child: Image.asset("images/uff.jpg", fit: BoxFit.cover),
+          ),
+          BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 2.0, sigmaY: 2.0),
+            child: Container(
+              color: Colors.black.withOpacity(0.3),
             ),
           ),
-        ),
-        Container(
-          width: double.infinity,
-          height: double.infinity,
-          decoration: BoxDecoration(color: Colors.black),
-          child: Image.asset("images/uff.jpg", fit: BoxFit.cover),
-        ),
-        BackdropFilter(
-          filter: ImageFilter.blur(sigmaX: 2.0, sigmaY: 2.0),
-          child: Container(
-            color: Colors.black.withOpacity(0.3),
+          Column(
+            children: [
+              Align(
+                alignment: FractionalOffset.topCenter,
+                child: Padding(
+                  padding: EdgeInsets.only(
+                      top: 80.0), // Adjust the top margin as desired
+                  child: Image.asset(
+                    "images/logouff.png",
+                    width: 200,
+                    height: 200,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Align(
+                  alignment: Alignment.center,
+                  child: Container(
+                    width: 392,
+                    height: 401,
+                    decoration: BoxDecoration(
+                      color: Color(0xFF262A2B).withOpacity(0.7),
+                      borderRadius: BorderRadius.circular(24),
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
-        ),
-        Align(
-          alignment: FractionalOffset.topCenter,
-          child: Padding(
-            padding: EdgeInsets.only(top: 40.0),
-            child: Image.asset(
-              "images/logouff.png",
-              width: 208.57,
-              height: 214.14,
-            ),
-          ),
-        ),
-      ],
-    ));
+        ],
+      ),
+    );
   }
 }

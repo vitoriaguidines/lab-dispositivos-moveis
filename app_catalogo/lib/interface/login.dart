e50358c019ac4ebaf8e3c3ea77a18afbd2bc7837
vitoriaguidines <vitoriaguidine@id.uff.br>
2023-07-15 19:47:41 -0300

    fundo borrado

--- app_catalogo/lib/interface/login.dart
+++ app_catalogo/lib/interface/login.dart
@@ -1,37 +1,35 @@
 //tela contem espaço para cadastro e cleber
+import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
 class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
+  const LoginPage({Key? key}) : super(key: key);
 
   @override
   State<LoginPage> createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
-  TextEditingController _emailController = TextEditingController();
-  TextEditingController _passwordController = TextEditingController();
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(10),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              width: 428,
-              height: 932,
-              child: Image.asset("images/fundo.jpg"),
+      body: Stack(
+        children: [
+          Container(
+            width: double.infinity,
+            height: double.infinity,
+            decoration: BoxDecoration(color: Colors.black),
+            child: Image.asset("images/uff.jpg", fit: BoxFit.cover),
+          ),
+          BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 2.0, sigmaY: 2.0),
+            child: Container(
+              color: Colors.black.withOpacity(0.3),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }

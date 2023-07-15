bbef3f96b6fbb4d489646777379be51677505e46
vitoriaguidines <vitoriaguidine@id.uff.br>
2023-07-15 20:05:32 -0300

    logo posicionada

--- app_catalogo/lib/interface/login.dart
+++ app_catalogo/lib/interface/login.dart
@@ -15,22 +15,42 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Container(
-            width: double.infinity,
-            height: double.infinity,
-            decoration: BoxDecoration(color: Colors.black),
-            child: Image.asset("images/uff.jpg", fit: BoxFit.cover),
-          ),
-          BackdropFilter(
-            filter: ImageFilter.blur(sigmaX: 2.0, sigmaY: 2.0),
-            child: Container(
-              color: Colors.black.withOpacity(0.3),
+        body: Stack(
+      children: [
+        Container(
+          width: 328,
+          height: 193,
+          decoration: ShapeDecoration(
+            color: Color(0xFF262A2B),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(24),
             ),
           ),
-        ],
-      ),
-    );
+        ),
+        Container(
+          width: double.infinity,
+          height: double.infinity,
+          decoration: BoxDecoration(color: Colors.black),
+          child: Image.asset("images/uff.jpg", fit: BoxFit.cover),
+        ),
+        BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 2.0, sigmaY: 2.0),
+          child: Container(
+            color: Colors.black.withOpacity(0.3),
+          ),
+        ),
+        Align(
+          alignment: FractionalOffset.topCenter,
+          child: Padding(
+            padding: EdgeInsets.only(top: 40.0),
+            child: Image.asset(
+              "images/logouff.png",
+              width: 208.57,
+              height: 214.14,
+            ),
+          ),
+        ),
+      ],
+    ));
   }
 }
